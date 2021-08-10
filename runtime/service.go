303a1b6b05e147e07296ee197e640ff0c91024ec
Amory Hoste <amory.hoste@gmail.com>
2021-08-10 20:47:27 +0200

    Add parameter to overwrite devmapper snapshot device when loading snapshots

--- runtime/service.go
+++ runtime/service.go
@@ -113,6 +113,7 @@ type loadSnapReq struct {
 	MemFilePath          string `json:"mem_file_path"`
 	SendSockAddr         string `json:"sock_file_path"`
 	EnableUserPageFaults bool   `json:"enable_user_page_faults"`
+	NewSnapshotPath		 string	`json:"new_snapshot_path"`
 }
 
 // implements shimapi
@@ -1809,7 +1810,7 @@ func formPauseReq() (*http.Request, error) {
 	return req, nil
 }
 
-func formLoadSnapReq(snapshotPath, memPath, sendSockAddr string, isUpf bool) (*http.Request, error) {
+func formLoadSnapReq(snapshotPath, memPath, sendSockAddr string, isUpf bool, newSnapshotPath string) (*http.Request, error) {
 	var req *http.Request
 
 	data := loadSnapReq{
@@ -1817,6 +1818,7 @@ func formLoadSnapReq(snapshotPath, memPath, sendSockAddr string, isUpf bool) (*h
 		MemFilePath:          memPath,
 		SendSockAddr:         sendSockAddr,
 		EnableUserPageFaults: isUpf,
+		NewSnapshotPath:           newSnapshotPath,
 	}
 
 	json, err := json.Marshal(data)
@@ -2045,7 +2047,7 @@ func (s *service) LoadSnapshot(ctx context.Context, req *proto.LoadSnapshotReque
 	}
 
 	s.logger.Debugf("Creating snap request")
-	loadSnapReq, err := formLoadSnapReq(req.SnapshotFilePath, req.MemFilePath, sendSockAddr, req.EnableUserPF)
+	loadSnapReq, err := formLoadSnapReq(req.SnapshotFilePath, req.MemFilePath, sendSockAddr, req.EnableUserPF, req.NewSnapshotPath)
 	if err != nil {
 		s.logger.WithError(err).Error("Failed to create load snapshot request")
 		return nil, err
